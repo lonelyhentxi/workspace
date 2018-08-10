b6132c9733f3dd79993b445a47ce393e6ecf1bbe
lonelyhentai <master@evernightfireworks.com>
2018-08-10 22:31:00 +0800

    espkg-try::rxjs::transform

--- espkg-try/src/app/rxjs/rxjs.module.ts
+++ espkg-try/src/app/rxjs/rxjs.module.ts
@@ -5,12 +5,13 @@ import { RxjsRoutingModule } from './rxjs-routing.module';
 import { RxjsCh1Component } from './rxjs-ch1/rxjs-ch1.component';
 import { RxjsCh4Component } from './rxjs-ch4/rxjs-ch4.component';
 import { RxjsCh5Component } from './rxjs-ch5/rxjs-ch5.component';
+import { RxjsCh8Component } from './rxjs-ch8/rxjs-ch8.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RxjsRoutingModule
   ],
-  declarations: [RxjsCh1Component, RxjsCh4Component, RxjsCh5Component]
+  declarations: [RxjsCh1Component, RxjsCh4Component, RxjsCh5Component,RxjsCh8Component]
 })
 export class RxjsModule { }
