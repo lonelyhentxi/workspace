1b48ab48f64bb922fd7665a85f3f6361e8e2b37c
lonelyhentai <lonely_hentai@hotmail.com>
2018-09-05 18:34:00 +0800

    emcpp::smart_ptr.finish()

--- cpp/emcpp/impls/smart_ptr.cpp
+++ cpp/emcpp/impls/smart_ptr.cpp
@@ -1,5 +1,7 @@
 #include <memory>
 #include <iostream>
+#include <unordered_map>
+#include "widget.h"
 
 namespace uptr {
     class Investment {
@@ -34,10 +36,25 @@ namespace uptr {
     }
 }
 
-namespace sptr {
-    class Widget : public std::enable_shared_from_this<Widget> {
-
+namespace wp {
+    class Widget {
+    public:
+        using WidgetId = int;
+        explicit Widget(WidgetId i):id_{std::forward<decltype(i)>(i)} {}
+    private:
+        WidgetId id_;
     };
+
+    std::shared_ptr<const Widget> fastLoadWidget(const Widget::WidgetId &id) {
+        static std::unordered_map<Widget::WidgetId,std::weak_ptr<const Widget>> cache;
+        auto objPtr = cache[id].lock();
+        if(!objPtr) {
+            objPtr = std::make_shared<Widget>(std::rand()%10000+1);
+            cache[id] = objPtr;
+        }
+        // 一般要添加过期机制
+        return objPtr;
+    }
 }
 
 int main() {
@@ -68,6 +85,41 @@ int main() {
         // std::make_shared 总是会创建一个控制块
         // 从专属控制权的指针生成时会创建一个控制块
         // 使用裸指针创建时候会创建一个控制块
+        // 没有 shared_ptr<T[]>
+    }
+    /**
+     * 对于类似 std::shared_ptr 但有可能空悬的指针使用 weak_ptr
+     */
+    // weak_ptr 依赖 shared_ptr 但是不占用引用计数
+    {
+        auto spw = std::make_shared<uptr::Investment>();
+        std::weak_ptr<uptr::Investment> wpw(spw);
+        spw = nullptr;
+        std::cout << "wpw has expired? - " << (wpw.expired() ? "true" : "false") << std::endl;
+        std::shared_ptr<uptr::Investment> spw1 = wpw.lock(); // 若 wpw 失效，则spw1为空，且这是一个“原子操作”
+        try {
+            std::shared_ptr<uptr::Investment> spw2{wpw}; // 若wpw失效，则抛出异常
+        } catch (std::exception &err) {
+            std::cout << err.what() << std::endl;
+        }
+        // 有时还用在观察者模式中判断指向观察者的指针是否空悬
+        // std::weak_ptr 有时还用来解决 std::shared_ptr 指涉有可能引起的环路问题
+        // 如果子节点的生存期不比父节点更长，那么由于不会出现空悬的情况，用 unique_ptr 即可
+    }
+    /**
+     * 优先使用 std::make_unique 和 std::make_shared 而不是直接使用 new
+     */
+    // 直接使用 new 运算符可能由于异常等原因导致内存泄漏
+    // make_shared 让编译器有机会利用更简洁的数据结构产生更小和更快的代码（make_xxx内存分配一次，new 本身和附加数据块各一次）
+    // make 系列工厂函数不允许使用自定义构造器
+    // make 对形参进行完美转发使用的是圆括号，但是大括号初始化物并不能完美转发，变通的方法是先用auto对initializer_list 初始化再传入
+    // 使用 make 系列对有自定义版本 new 和 delete 操作符使用并不是好主义
+    /**
+     * 使用 pimpl 习惯用法时，将特殊成员函数的定义放在实现文件中
+     */
+    {
+        using namespace sptr_pimpl;
+        Widget widget;
     }
 }
 
