296ed9bcea643386a8f1ebb72ad30027eb407fe4
lonelyhentai <master@evernightfireworks.com>
2018-09-25 09:33:00 +0800

    data_mining::comp3009::homework1

--- data_mining/comp3009/src/lab1/statistics_service.py
+++ data_mining/comp3009/src/lab1/statistics_service.py
@@ -2,6 +2,10 @@ from typing import Collection, Any, Dict, Tuple
 from functools import reduce
 from math import sqrt, inf
 from shared.type_def import Numeric
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+from typing import Callable
 
 
 class StatisticsService:
@@ -56,3 +60,34 @@ class StatisticsService:
         agg = self.aggregate(series)
         density_sum: float = self.sum(agg.values())
         return list(zip(agg.keys(), map(lambda x: x / density_sum, agg.values())))
+
+    def make_factor_indexes(self, series: Collection[Any]) -> Dict[Any, int]:
+        unique = self.unique(series)
+        ret: Dict[Any, int] = {}
+        for index, record in enumerate(unique):
+            ret[record] = index
+        return ret
+
+    def corrdot_gen(self, corr_method: str) -> Callable:
+
+        def _corrdot(*args, **kwargs):
+            df: pd.DataFrame = args[0]
+            corr_r = df.corr(args[1], corr_method)
+            corr_text = f"{corr_r:2.2f}".replace("0.", ".")
+            ax = plt.gca()
+            ax.set_axis_off()
+            marker_size = abs(corr_r) * 10000
+            ax.scatter(.5, .5, marker_size, corr_r, alpha=0.6, cmap="coolwarm",
+                       vmin=-1, vmax=1, transform=ax.transAxes)
+            font_size = abs(corr_r) * 40 + 5
+            ax.annotate(corr_text, [.5, .5, ], xycoords="axes fraction",
+                        ha='center', va='center', fontsize=font_size)
+
+        return _corrdot
+
+    def draw_corr(self, df: pd.DataFrame, corr_method: str) -> sns.PairGrid:
+        g = sns.PairGrid(df, aspect=1.4, diag_sharey=False)
+        g.map_lower(sns.regplot, order=3, ci=False, line_kws={'color': 'black'})
+        g.map_diag(sns.distplot, kde_kws={'color': 'black'})
+        corrdot = self.corrdot_gen(corr_method)
+        return g.map_upper(corrdot)
--- data_mining/comp3009/src/lab1/user_account_service.py
+++ data_mining/comp3009/src/lab1/user_account_service.py
@@ -9,6 +9,7 @@ from os import path
 from lab1.statistics_service import StatisticsService
 from typing import Collection, Any
 from shared.type_def import Numeric
+import re
 
 
 class UserAccountService:
@@ -26,6 +27,15 @@ class UserAccountService:
         raw_df: DataFrame = pandas.read_csv(csv_path, sep=";", encoding="utf-8")
         self.logger.info(self.log_render("read csv file from {0}".format(csv_path)))
         return raw_df
+
+    def is_numeric_series(self, series: pandas.Series) -> bool:
+        if isinstance(series, pandas.Series):
+            return bool(re.search(r'(int|float)\d*', str(series.dtype), flags=re.I))
+        else:
+            for record in series:
+                if not isinstance(record, int) and not isinstance(record, float):
+                    return False
+            return True
 
     def numeric_describe(self, series: Collection[Numeric], series_name: str):
         methods = ['mean', 'max', 'min', 'range', 'std', 'ustd', 'var', 'uvar']
@@ -49,3 +59,18 @@ class UserAccountService:
             self.logger.info(self.log_render(
                 'freq - {} {}'.format(str(k), v)
             ))
+
+    def make_indexed_factor(self, series: pandas.Series) -> pandas.Series:
+        dic = self.statistics_service.make_factor_indexes(series)
+        lst: list = map(lambda x: dic[x], series)
+        copy_series = pandas.Series(data=lst)
+        return copy_series
+
+    def make_indexed_factors(self, df: DataFrame) -> DataFrame:
+        copy_df = df.copy()
+        for col in df.columns.get_values():
+            if self.is_numeric_series(df[col]):
+                pass
+            else:
+                copy_df[col] = self.make_indexed_factor(copy_df[col])
+        return copy_df
