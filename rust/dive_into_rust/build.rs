539c0025e172aec113aa9dd087b3254c0d708702
lonelyhentai <lonely_hentai@hotmail.com>
2018-10-06 06:38:56 +0800

    dive_into_rust::error

--- rust/dive_into_rust/build.rs
+++ rust/dive_into_rust/build.rs
@@ -7,18 +7,17 @@ use std::panic;
 
 fn main() {
     panic::catch_unwind(|| {
+        let out_dir = env::var("OUT_DIR").unwrap();
+        let dest_path  = Path::new(&out_dir).join("commit_id.rs");
+        let mut f = File::create(&dest_path).unwrap();
 
+        let commit = Command::new("git")
+            .arg("rev-parse")
+            .arg("HEAD")
+            .output()
+            .expect("Failed to execute git command");
+        let commit  = String::from_utf8(commit.stdout).expect("Invalid utf8 string");
+        let output = format!(r#"pub const CURRENT_COMMIT_ID: &'static str = "{}";"#,commit);
+        f.write_all(output.as_bytes()).unwrap();
     }).unwrap_or_else(|_| {println!("There have a panic, but just a try")});
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let dest_path  = Path::new(&out_dir).join("commit_id.rs");
-    let mut f = File::create(&dest_path).unwrap();
-
-    let commit = Command::new("git")
-        .arg("rev-parse")
-        .arg("HEAD")
-        .output()
-        .expect("Failed to execute git command");
-    let commit  = String::from_utf8(commit.stdout).expect("Invalid utf8 string");
-    let output = format!(r#"pub const CURRENT_COMMIT_ID: &'static str = "{}";"#,commit);
-    f.write_all(output.as_bytes()).unwrap();
 }
