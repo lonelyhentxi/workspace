1992976c7e8aaf92c1b3c5c2d2b877ac190ebc4b
lonelyhentai <master@evernightfireworks.com>
2018-10-20 21:19:40 +0800

    essential_cs::classes::{Common,Self,SelfExtension}

---
+++ dotnet/essential_cs/essential_cs/classes/Self.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace essential_cs.classes
+{
+    public class Self
+    {
+        public Self() {}
+
+        public bool IsSelf()
+        {
+            return true;
+        }
+    }
+}
