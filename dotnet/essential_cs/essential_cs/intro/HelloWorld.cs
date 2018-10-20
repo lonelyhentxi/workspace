e5a62e9099b81cc2c229120bfeca870596251cdc
lonelyhentai <master@evernightfireworks.com>
2018-10-20 13:00:36 +0800

    essential_cs::{init,intro}

---
+++ dotnet/essential_cs/essential_cs/intro/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace essential_cs.intro
+{
+    class HelloWorld
+    {
+       /* static void Main()
+        {
+            System.Console.WriteLine("Hello. My name is lonelyhentai");
+        }
+        */
+    }
+}
