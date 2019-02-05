d36b47a0893b7b4879c12a1aeac76da2ec4ca2ce
lonelyhentai <master@evernightfireworks.com>
2019-02-05 19:37:30 +0800

    temp::20190205

---
+++ practice/codewars/src/fourkyu/AddingBigNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace Codewars.Fourkyu
+{
+    public class AddingBigNumbers
+    {
+        internal readonly int _sign;
+        internal readonly uint[] _bits;
+
+        public AddingBigNumbers(string s)
+        {
+            int smallIntResult;
+            if (int.TryParse(s, out smallIntResult))
+            {
+                if (smallIntResult > int.MinValue)
+                {
+                    _sign = smallIntResult;
+                    _bits = (uint[]) null;
+                }
+            }
+        }
+
+
+
+        public static string Add(string lhs, string rhs)
+        {
+            var lArray = lhs.Select(ch => uint.Parse(ch.ToString())).ToArray();
+            var rArray = rhs.Select(ch => uint.Parse(ch.ToString())).ToArray();
+            var minLength = Math.Min(lArray.Length, rArray.Length);
+            var lIndex = lArray.Length;
+            var rIndex = rArray.Length;
+            var target = rhs.
+            for (var i = minLength - 1; i >= 0; i--)
+            {
+                BigInteger a = 1;
+                BigInteger b = 2;
+                var c = a + b;
+            }
+        }
+    }
+}
