ff61bd6ea502f6fb6c84c67815cb0a7bed8f0c33
lonelyhentai <master@evernightfireworks.com>
2019-02-06 23:20:05 +0800

    codewars::{valid_parentheses,adding_big_numbers}

---
+++ practice/codewars/src/fivekyu/ValidParentheses.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Codewars.Fivekyu
+{
+    public static class Parentheses
+    {
+        public static readonly HashSet<char> OpenParentheses = new HashSet<char> {'(', '[', '<'};
+        public static readonly HashSet<char> CloseParentheses = new HashSet<char> {')', ']', '>'};
+
+        public static readonly Dictionary<char, char> Pairs = new Dictionary<char, char>
+            {{'(', ')'}, {')', '('}, {'[', ']'}, {']', '['}, {'>', '<'}, {'<', '>'}};
+
+        public static bool ValidParentheses(string input)
+        {
+            var stack = new Stack<char>();
+            foreach (var ch in input)
+            {
+                if (OpenParentheses.Contains(ch))
+                {
+                    stack.Push(ch);
+                }
+                else if (CloseParentheses.Contains(ch))
+                {
+                    try
+                    {
+                        var last = stack.Pop();
+                        if (Pairs[ch] != last)
+                        {
+                            return false;
+                        }
+                    }
+                    catch(InvalidOperationException)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+
+    [TestFixture]
+    public class ParenthesesTest
+    {
+        [Test]
+        public void SampleTest1()
+        {
+            Assert.AreEqual(true, Parentheses.ValidParentheses("()"));
+        }
+
+        [Test]
+        public void SampleTest2()
+        {
+            Assert.AreEqual(false, Parentheses.ValidParentheses(")(((("));
+        }
+    }
+}
