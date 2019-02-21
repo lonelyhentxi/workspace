c114a343665d31c8be9e223fb5b0913bd8d7096c
lonelyhentai <master@evernightfireworks.com>
2019-02-22 02:55:27 +0800

    practice::csp::init

---
+++ practice/csp/src/201312/most_digit.cpp
@@ -0,0 +1,11 @@
+#include "most_digit.hpp"
+#include <cassert>
+using namespace csp::p1312;
+
+int main() {
+	int32_t size = 6;
+	auto numbers = vector<int32_t>{10,1,10,20,30,20};
+	int32_t expected = 10;
+	assert(most_digit(size,numbers)==expected);
+	return 0;
+}
---
+++ practice/csp/src/test.cpp
@@ -0,0 +1,4 @@
+#define CATCH_CONFIG_MAIN
+#ifdef WITH_CMAKE
+#include <catch2/catch.hpp>
+#endif
