f20b788df8d9fe2e69c2da5d12b095f8608604a3
lonelyhentai <master@evernightfireworks.com>
2019-03-16 18:21:00 +0800

    comp3010::exp2::fi

--- database/comp3010/exp2/src/index.ts
+++ database/comp3010/exp2/src/index.ts
@@ -1,121 +1,325 @@
+// for args validation
 import { isInteger, isNil } from 'lodash';
-import mysql from 'mysql';
-import { type } from "os";
+// for database connection
+import * as mysql from 'mysql';
+// for args parse
+import program from 'commander';
 
-const argv = require('yargs').argv;
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'company',
-    password: 'Zh123123en'
-});
-connection.connect((err) => {
-    if (err) {
-        console.log(`无法连接数据库: ${err}`);
-    } else {
-        console.log('连接到数据库');
+async function main() {
+    try {
+        program
+            .version('0.1.0')
+            .option('-q,--question <question_id>', 'id of the question to choose', parseInt)
+            .option('-p,--params <params_json>', 'json string format params to pass', JSON.parse)
+            .parse(process.argv);
+    } catch (e) {
+        console.log(e);
+        console.error('invalid args.');
+        process.exit(-1);
     }
-});
 
+    const connection = mysql.createConnection({
+        host: '192.168.6.52',
+        user: 'root',
+        database: 'company',
+        password: 'test'
+    });
 
-const option = {question: argv.q - 1, params: argv.q};
-const questionId: number = option['question'];
+    const exit = async () => {
+        await connection.end();
+        process.exit(0);
+    };
 
-if (!isInteger(questionId) || questionId < 0 || questionId > 3) {
-    console.log(`你的问题序号是${questionId + 1}, 但是应在1-4之间.`);
-}
-if (isNil(option['params'])) {
-    console.log(`请输入json格式的参数`);
-}
-try {
-    option.params = JSON.parse(argv.p);
-} catch {
-    console.log(`请输入json格式的参数`);
-}
-const actions = [
-    (queries: { pno: string }) => {
-        if((typeof queries.pno) != 'string') {
-            console.log(`请输入正确的pno`);
-            connection.end(()=>{});
-        }
-        connection.query(
-            'SELECT essn FROM works_on WHERE pno = ?',
-            [queries.pno],
-            function (err, results) {
-                console.log('查询结果为：');
-                for (const res of results) {
-                    console.log(`${res.essn}工作于项目${queries.pno}。`);
-                }
-                connection.end(()=>{});
-            }
-        );
-    },
-    (queries: { pname: string }) => {
-        if((typeof queries.pname) != 'string') {
-            console.log(`请输入正确的pname`);
-            connection.end(()=>{});
-        }
-        connection.query(
-            `select distinct ename from EMPLOYEE where essn in (select distinct essn from works_on natural join project where pname = ?)`,
-            [queries.pname],
-            function (err, results) {
-                if (err) {
-                    console.log('查询失败');
-                }
-                console.log('查询结果为：');
-                for (const res of results) {
-                    console.log(`${res.ename}工作于项目${queries.pname}。`);
-                }
-                connection.end(()=>{});
-            }
-        );
-    },
-    (queries: { dname: string }) => {
-        if((typeof queries.dname) != 'string') {
-            console.log(`请输入正确的dname`);
-            connection.end(()=>{});
-        }
-        connection.query(
-            'SELECT DISTINCT ename, address FROM employee NATURAL JOIN department WHERE dname = ?',
-            [queries.dname],
-            function (err, results) {
-                if (err) {
-                    console.log('查询失败');
-                }
-                console.log('查询结果为：');
-                for (const res of results) {
-                    console.log(`名为${res.ename}，地址为${res.address}的员工，工作于${queries.dname}。`);
-                }
-                connection.end(()=>{});
-            }
-        );
-    },
-    (queries: { dname: string, salary: number }) => {
-        if((typeof queries.dname) != 'string') {
-            console.log(`请输入正确的dname`);
-            connection.end(()=>{});
-        }
-        if(!isInteger(queries.salary)) {
-            console.log(`请输入正确的salary`);
-            connection.end(()=>{});
-        }
-        connection.query(
-            'SELECT DISTINCT ename, address FROM employee NATURAL JOIN department WHERE dname = ? AND salary < ?',
-            [queries.dname, queries.salary],
-            function (err, results) {
-                if (err) {
-                    console.log('查询失败');
-                }
-                console.log('查询结果为：');
-                for (const res of results) {
-                    console.log(`名为${res.ename}，地址为${res.address}的员工，工作于${queries.dname},且薪水小于${queries.salary}`);
-                }
-                connection.end(()=>{});
-            }
-        );
+    try {
+        await connection.connect();
+        console.log(`connected.`);
+    } catch (err) {
+        console.error(`failed to connect: ${err}`);
+        await exit();
     }
-];
-// @ts-ignore
-actions[questionId](option.params);
+
+    const wrappedQuery = async function (sql: string, args: any[]) {
+        return new Promise((resolve, reject) => {
+            connection.query(sql, args, (err, results) => {
+                if (err) {
+                    return reject(err);
+                } else {
+                    return resolve(results);
+                }
+            });
+        });
+    } as (sql: string, args: any[]) => Promise<any[]>;
+
+    const actions = [
+        // no 1
+        async (queries: { pno: string }) => {
+            if ((typeof queries.pno) != 'string') {
+                console.error(`please input correct string as pno.`);
+                await exit();
+            }
+            try {
+                const results: { essn: string }[] = await wrappedQuery(
+                    'SELECT essn FROM works_on WHERE pno = ?',
+                    [queries.pno],
+                );
+                console.log('results are：');
+                for (const res of results) {
+                    console.log(`employee ${res.essn} works on project ${queries.pno}.`);
+                }
+            } catch (e) {
+                console.error(`query failed: ${e}`);
+            } finally {
+                await exit();
+            }
+        },
+        // no 2
+        async (queries: { pname: string }) => {
+            if ((typeof queries.pname) != 'string') {
+                console.error(`please input correct string as pname.`);
+                await exit();
+            }
+            try {
+                const results: { ename: string }[] = await wrappedQuery(
+                    `SELECT DISTINCT ename FROM employee WHERE essn IN (SELECT DISTINCT essn FROM works_on NATURAL JOIN project WHERE pname = ?)`,
+                    [queries.pname],
+                );
+                console.log('results are：');
+                for (const res of results) {
+                    console.log(`employee ${res.ename} works on project ${queries.pname}.`);
+                }
+            } catch (e) {
+                console.error(`query failed: ${e}`);
+            } finally {
+                await exit();
+            }
+        },
+        // no 3
+        async (queries: { dname: string }) => {
+            if ((typeof queries.dname) != 'string') {
+                console.error(`please input correct string as dname`);
+                await exit();
+            }
+            try {
+                const results: { ename: string, address: string }[] = await wrappedQuery(
+                    'SELECT DISTINCT ename, address FROM employee NATURAL JOIN department WHERE dname = ?',
+                    [queries.dname],
+                );
+                console.log('results are：');
+                for (const res of results) {
+                    console.log(`employee ${res.ename} works on project ${queries.dname}, and his/her address is ${res.address}.`);
+                }
+            } catch (e) {
+                console.error(`query failed: ${e}`);
+            } finally {
+                await exit();
+            }
+        },
+        // no 4
+        async (queries: { dname: string, salary: number }) => {
+            if ((typeof queries.dname) != 'string') {
+                console.error(`please input correct string as dname`);
+                await exit();
+            }
+            if (!isInteger(queries.salary)) {
+                console.log(`please input correct integer as salary`);
+                await exit();
+            }
+            try {
+                const results: { ename: string, address: string }[] = await wrappedQuery(
+                    'SELECT DISTINCT ename, address FROM employee NATURAL JOIN department WHERE dname = ? AND salary < ?',
+                    [queries.dname, queries.salary],
+                );
+                console.log('results are：');
+                for (const res of results) {
+                    console.log(`employee ${res.ename} works on project ${queries.dname}.\n` +
+                        `and his/her address is ${res.address}, and his/her salary is lower than ${queries.salary}.`);
+                }
+            } catch (e) {
+                console.error(`query failed: ${e}`);
+            } finally {
+                await exit();
+            }
+        },
+        // no 5
+        async (queries: { pno }) => {
+            if ((typeof queries.pno) != 'string') {
+                console.error(`please input correct string as pno`);
+                await exit();
+            }
+            try {
+                const results: { ename: string }[] = await wrappedQuery(
+                    `SELECT employee.ename
+                        FROM employee
+                        WHERE employee.essn
+                                NOT IN (
+                                SELECT essn as essn
+                                FROM works_on
+                                WHERE pno = ?
+                              );`,
+                    [queries.pno],
+                );
+                console.log('results are：');
+                for (const res of results) {
+                    console.log(`employee ${res.ename} doesn't work on project ${queries.pno}.`);
+                }
+            } catch (e) {
+                console.error(`query failed: ${e}`);
+            } finally {
+                await exit();
+            }
+        },
+        // no 6
+        async (queries: { ename }) => {
+            if ((typeof queries.ename) != 'string') {
+                console.error(`please input correct string as ename`);
+                await exit();
+            }
+            try {
+                const results: { ename: string, dname: string }[] = await wrappedQuery(
+                    `SELECT DISTINCT ename, dname
+                        FROM employee
+                               NATURAL JOIN department
+                        WHERE superssn = (
+                          SELECT essn
+                          FROM employee
+                          WHERE ename = ?
+                          );`,
+                    [queries.ename],
+                );
+                console.log('results are：');
+                for (const res of results) {
+                    console.log(`employee ${res.ename} work on ${res.dname} and his/hers boss is ${queries.ename}.`);
+                }
+            } catch (e) {
+                console.error(`query failed: ${e}`);
+            } finally {
+                await exit();
+            }
+        },
+        // no 7
+        async (queries: { pno1, pno2 }) => {
+            if ((typeof queries.pno1) != 'string') {
+                console.error(`please input correct string as pno1`);
+                await exit();
+            }
+            if ((typeof queries.pno2) != 'string') {
+                console.error(`please input correct string as pno2`);
+                await exit();
+            }
+            if (queries.pno1 == queries.pno2) {
+                console.error(`please input two different project`);
+            }
+            try {
+                const results: { essn: string }[] = await wrappedQuery(
+                    `SELECT essn
+                        FROM (
+                               (SELECT essn
+                                FROM works_on
+                                WHERE pno = ? )
+                               UNION ALL
+                               (
+                                 SELECT essn
+                                 FROM works_on
+                                 WHERE pno = ?
+                               )
+                             )
+                               AS union_essn
+                        GROUP BY (essn)
+                        HAVING COUNT(essn) >= 2;`,
+                    [queries.pno1, queries.pno2],
+                );
+                console.log('results are：');
+                for (const res of results) {
+                    console.log(`employee ${res.essn} at least work on project ${queries.pno1} and ${queries.pno2}.`);
+                }
+            } catch (e) {
+                console.error(`query failed: ${e}`);
+            } finally {
+                await exit();
+            }
+        },
+        // no 8
+        async (queries: { salary }) => {
+            if (!isInteger(queries.salary)) {
+                console.error(`please input correct integer as salary`);
+                await exit();
+            }
+            try {
+                const results: { dname: string }[] = await wrappedQuery(
+                    `SELECT DISTINCT department.dname
+                        FROM department,
+                             (
+                               SELECT employee.dno
+                               FROM employee
+                               GROUP BY (employee.dno)
+                               HAVING AVG(employee.salary) < ?
+                             ) AS dep_avg_salary
+                        WHERE department.dno = dep_avg_salary.dno;`,
+                    [queries.salary],
+                );
+                console.log('results are：');
+                for (const res of results) {
+                    console.log(`the average salary of department ${res.dname} is lower than ${queries.salary}.`);
+                }
+            } catch (e) {
+                console.error(`query failed: ${e}`);
+            } finally {
+                await exit();
+            }
+        },
+        // no 9
+        async (queries: { n, hours }) => {
+            if (!isInteger(queries.n)) {
+                console.error(`please input correct integer as n`);
+                await exit();
+            }
+            if (!isInteger(queries.hours)) {
+                console.error(`please input correct integer as hours`);
+                await exit();
+            }
+            try {
+                const results: { ename: string }[] = await wrappedQuery(
+                    `SELECT DISTINCT employee.ename
+                            FROM employee,
+                                 (
+                                   SELECT works_on.essn
+                                   FROM works_on
+                                   GROUP BY works_on.essn
+                                   HAVING COUNT(works_on.pno) >= ?
+                                      AND SUM(works_on.hours) <= ?
+                                 ) AS selected_employee
+                            WHERE employee.essn = selected_employee.essn;`,
+                    [queries.n, queries.hours],
+                );
+                console.log('results are：');
+                for (const res of results) {
+                    console.log(`employee ${res.ename} work project is greater than or equal to ${queries.hours},\n` +
+                        `and hours is lower than or equal to ${queries.hours}.`);
+                }
+            } catch (e) {
+                console.error(`query failed: ${e}`);
+            } finally {
+                await exit();
+            }
+        },
+    ];
+    // take out args
+    const question: number = program['question'];
+    const params: any = program['params'];
+    // validate question id
+    if (!isInteger(question) || question < 1 || question > actions.length) {
+        console.error(`your question id is ${question + 1}, but should be 1-${actions.length}.`);
+        await exit();
+    }
+    // validate params (basically)
+    if (isNil(params)) {
+        console.error(`your pass nil as params, should be a json string`);
+        await exit();
+    }
+    await actions[question - 1](params);
+}
+
+main().then(() => {
+
+});
 
