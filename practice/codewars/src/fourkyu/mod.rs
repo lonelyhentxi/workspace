86695eb75ae3774421eaa40fa905db01023a97a3
lonelyhentai <master@evernightfireworks.com>
2019-03-17 12:36:06 +0800

    codewars::recover_a_secret_string_from_random_triplets

--- practice/codewars/src/fourkyu/mod.rs
+++ practice/codewars/src/fourkyu/mod.rs
@@ -5,3 +5,4 @@ pub mod decode_the_morse_code_advanced;
 pub mod sum_by_factors;
 pub mod number_of_integer_partitions;
 pub mod getting_along_with_integer_partitions;
+pub mod recover_a_secret_string_from_random_triplets;
