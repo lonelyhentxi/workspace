a3186c1633af4089d31bc5e46d52577040161660
lonelyhentai <master@evernightfireworks.com>
2019-03-26 20:09:16 +0800

    nlp_practice::tf_idf

--- data_ai/nlp_practice/lyrics/data/__init__.py
+++ data_ai/nlp_practice/lyrics/data/__init__.py
@@ -8,6 +8,10 @@ SONG_DATA_LINK = "https://raw.githubusercontent.com/GrayRobert/" \
                  "big-data-project/master/src/main/resources/temp/data/songdata.csv"
 SONG_DATA_MD5 = "7b5072c4cb7c84d89798592f1635bfc8"
 
+STOP_WORD_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "stopwords.txt")
+STOP_WORD_LINK = "https://raw.githubusercontent.com/kavgan/nlp-in-practice/master/tf-idf/resources/stopwords.txt"
+STOP_WORD_MD5 = "742a346fbcc61b8e899917b53a53dfb1"
+
 
 def md5(path: str):
     hash_md5 = hashlib.md5()
@@ -19,19 +23,17 @@ def md5(path: str):
 
 def get_lyrics():
     if not os.path.exists(SONG_DATA_PATH) or md5(SONG_DATA_PATH) != SONG_DATA_MD5:
-        try:
-            print(f"lyrics \"songdata.csv\" not exist, try download from {SONG_DATA_LINK}")
-            r = requests.get(SONG_DATA_LINK, stream=True)
-            with open(SONG_DATA_PATH, 'wb') as f:
-                total_length = int(r.headers.get('content-length'))
-                for chunk in progress.bar(r.iter_content(chunk_size=1024), label="Downloading data",
-                                          expected_size=(total_length / 1024) + 1):
-                    if chunk:
-                        f.write(chunk)
-                        f.flush()
-        except Exception as e:
-            print(e)
+        if os.path.exists(SONG_DATA_PATH):
             os.remove(SONG_DATA_PATH)
+        print(f"lyrics \"songdata.csv\" not exist, try download from {SONG_DATA_LINK}")
+        r = requests.get(SONG_DATA_LINK, stream=True)
+        with open(SONG_DATA_PATH, 'wb') as f:
+            total_length = int(r.headers.get('content-length'))
+            for chunk in progress.bar(r.iter_content(chunk_size=1024), label="Downloading data",
+                                      expected_size=(total_length / 1024) + 1):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
     all_songs = []
     with open(SONG_DATA_PATH, 'r') as f:
         content = f.readlines()
@@ -40,8 +42,30 @@ def get_lyrics():
             if line.rstrip("\n") == "\"":
                 current_a_song_str: str = "".join(current_a_song)
                 all_songs.append(current_a_song_str)
+            else:
+                current_a_song.append(line)
     return all_songs
+
+
+def get_stop_words():
+    if not os.path.exists(STOP_WORD_PATH) or md5(STOP_WORD_PATH) != STOP_WORD_MD5:
+        if os.path.exists(STOP_WORD_PATH):
+            os.remove(STOP_WORD_PATH)
+        print(f"\"stopwords.txt\" not exist, try download from {STOP_WORD_LINK}")
+        r = requests.get(STOP_WORD_LINK, stream=True)
+        with open(STOP_WORD_PATH, 'wb') as f:
+            total_length = int(r.headers.get('content-length'))
+            for chunk in progress.bar(r.iter_content(chunk_size=1024), label="Downloading data",
+                                      expected_size=(total_length / 1024) + 1):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+    with open(STOP_WORD_PATH, "r", encoding="utf-8") as f:
+        stopwords = f.readlines()
+        stop_set = set(m.strip() for m in stopwords)
+        return frozenset(stop_set)
 
 
 if __name__ == "__main__":
     print(md5(SONG_DATA_PATH))
+    print(md5(STOP_WORD_PATH))
--- data_ai/nlp_practice/lyrics/extract_keyword.py
+++ data_ai/nlp_practice/lyrics/extract_keyword.py
@@ -1,30 +1,70 @@
-from lyrics.data import get_lyrics
-from typing import Iterable
-
+from lyrics.data import get_lyrics, get_stop_words
+from typing import Iterable, Dict
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn
-from sklearn.cluster import KMeans
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from sklearn.decomposition import PCA
-from sklearn.decomposition import TruncatedSVD
-import nltk
-from wordcloud import WordCloud
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.dummy import DummyClassifier
-from sklearn.decomposition import LatentDirichletAllocation
+import re
 
-def extract(lyrics: Iterable[str]):
 
-    return [""]
+def pre_process(text: str) -> str:
+    text = text.lower()
+    text = re.sub("</?.*?>", " <> ", text)
+    text = re.sub("(\\d|\\W)+", " ", text)
+    return text
+
+
+def sort_coo(coo_matrix):
+    tuples = zip(coo_matrix.col, coo_matrix.data)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+
+
+def extract_topn_from_vector(feature_names, sorted_items, topn=5):
+    """get the feature names and tf-idf score of top n items"""
+
+    sorted_items = sorted_items[:topn]
+
+    score_vals = []
+    feature_vals = []
+
+    for idx, score in sorted_items:
+        fname = feature_names[idx]
+        score_vals.append(round(score, 3))
+        feature_vals.append(feature_names[idx])
+    results = {}
+    for idx in range(len(feature_vals)):
+        results[feature_vals[idx]] = score_vals[idx]
+
+    return results
+
+
+class TextExtractor:
+
+    def __init__(self):
+        self.has_fit = False
+        self.docs = None
+        self.cv = None
+        self.word_count_vec = None
+        self.tfidf_transformer = TfidfTransformer(smooth_idf=True, use_idf=True)
+        self.tf_idf_vec = None
+
+    def fit(self, texts: Iterable[str]) -> None:
+        if self.has_fit:
+            raise Exception("This extractor has been fit")
+        self.docs = list(map(lambda t: pre_process(t), texts))
+        stop_words = get_stop_words()
+        self.cv = CountVectorizer(max_df=0.85, stop_words=stop_words)
+        self.word_count_vec = self.cv.fit_transform(self.docs)
+        self.tfidf_transformer.fit(self.word_count_vec)
+
+    def predict(self, text: str) -> Dict:
+        tf_idf_vector = self.tfidf_transformer.transform(self.cv.transform([pre_process(text)]))
+        sorted_items = sort_coo(tf_idf_vector.tocoo())
+        keywords = extract_topn_from_vector(self.cv.get_feature_names(), sorted_items, 5)
+        return keywords
 
 
 if __name__ == "__main__":
     songs = get_lyrics()
     print("load file success.")
-    res = extract(songs)
-    print(res[0])
+    extractor = TextExtractor()
+    extractor.fit(songs)
+    print(extractor.predict(songs[0]))
