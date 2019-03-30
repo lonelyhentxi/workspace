9aa8beb9871fc3e38eef66c810e5c9491fd9e6a0
lonelyhentai <master@evernightfireworks.com>
2019-03-30 12:28:39 +0800

    comp3010::eru::temp_commit

---
+++ database/comp3010/exp3/Eru/Eru.Server/Interfaces/ICachingProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eru.Server.Interfaces
+{
+    public interface ICachingProvider
+    {
+        object Get(string key);
+        void Set(string key, object value);
+    }
+}
--- database/comp3010/exp3/Eru/Eru.Server/Startup.cs
+++ database/comp3010/exp3/Eru/Eru.Server/Startup.cs
@@ -1,127 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Eru.Server.Models;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Swashbuckle.AspNetCore.Swagger;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
-using AutoMapper;
-using Eru.Server.Utils;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using log4net.Repository;
+using log4net;
+using log4net.Config;
+using System.IO;
+
+#region CUSTOM_IMPORT
+
+using Eru.Server.Models;
+
+#endregion
 
 namespace Eru.Server
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+        public static ILoggerRepository repository { get; set; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            repository = LogManager.CreateRepository("eru");
+            XmlConfigurator.Configure(repository, new FileInfo("log4net.config.xml"));
         }
 
-        public IConfiguration Configuration { get; }
-
-        public void ConfigAuthServices(IServiceCollection services)
-        {
-            var appSettingsSection = Configuration.GetSection("AppSettings");
-            services.Configure<AppSettings>(appSettingsSection);
-            var appSettings = appSettingsSection.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-            services.AddAuthentication(x =>
-                {
-                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(x =>
-                {
-                    x.RequireHttpsMetadata = false;
-                    x.SaveToken = true;
-                    x.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(key),
-                        ValidateIssuer = false,
-                        ValidateAudience = false,
-                    };
-                });
-            // services.AddScoped<I>()
-        }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region CACHE_INJECTION
+
+            services.AddScoped<ICaching,>
+
+            #endregion
+            #region DB_CONTEXT_POOL_SERVICE
+
             services.AddDbContextPool<EruContext>(opt => { opt.UseInMemoryDatabase("eru"); });
+
+            #endregion
+
+            #region MVC_SERVICE
 
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
                 .AddNewtonsoftJson(options =>
-                    {
-                        options.SerializerSettings.ContractResolver = new DefaultContractResolver();
-                        options.SerializerSettings.Formatting = Formatting.Indented;
-                    }
-                );
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Info
                 {
-                    Title = "Eru API", Version = "v1",
-                    Description = "Eru Blog System API Documentation",
-                    TermsOfService = "None",
-                    Contact = new Contact
-                    {
-                        Name = "Zhou Yeheng",
-                        Email = "master@evernightfireworks.com"
-                    },
-                    License = new License
-                    {
-                        Name = "Common Creative 3.0",
-                        Url = "https://creativecommons.org/licenses/by/3.0/"
-                    }
+                    options.SerializerSettings.ContractResolver =
+                        new DefaultContractResolver();
+                    options.SerializerSettings.Formatting = Formatting.Indented;
                 });
-            });
+
+            #endregion
         }
 
-        public void AutoMapperConfigure(IApplicationBuilder app)
-        {
-            Mapper.Initialize(cfg => { });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            /*
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            */
 
-            // app.UseHttpsRedirection();
-            // Enable middleware to serve generated Swagger as a Json endpoint
-            app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/doc/v1/swagger.json", "Eru API v1"); });
-            app.UseAuthentication();
             app.UseMvc();
-        }
+        } 
     }
 }
