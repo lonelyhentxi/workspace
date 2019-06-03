8f2604e54405252981427282f750c5d2a7489b54
lonelyhentai <master@evernightfireworks.com>
2019-06-03 17:52:43 +0800

    learnlibs::libgit2::{clone,commit,diff,add,init}

---
+++ dotnet/LearnLibs/RepoUtils.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace LearnLibs
+{
+    static class RepoUtils
+    {
+        public static void FsSafeCopy(string relativePath, string sourceBase, string targetBase)
+        {
+            var sourceFullPath = Path.Combine(sourceBase, relativePath);
+            var targetFullPath = Path.Combine(targetBase, relativePath);
+            var targetDirectory = Path.GetDirectoryName(targetFullPath);
+            if (!Directory.Exists(targetDirectory))
+            {
+                Directory.CreateDirectory(targetDirectory);
+            }
+
+            if (File.Exists(sourceFullPath))
+            {
+                File.Create(targetFullPath).Close();
+                File.Copy(sourceFullPath, targetFullPath, true);
+            }
+            else if (Directory.Exists(sourceFullPath))
+            {
+                Directory.CreateDirectory(targetFullPath);
+            }
+        }
+
+        private static void _fsSafeDeleteFile(string path)
+        {
+            File.SetAttributes(path, FileAttributes.Normal);
+            File.Delete(path);
+        }
+
+        private static void _fsSafeDelete(string path)
+        {
+            if (File.Exists(path))
+            {
+                _fsSafeDeleteFile(path);
+            }
+            else if (Directory.Exists(path))
+            {
+                foreach (var e in Directory.EnumerateFileSystemEntries(path))
+                {
+                    _fsSafeDelete(Path.Combine(path, e));
+                }
+
+                Directory.Delete(path, true);
+            }
+        }
+
+        public static void FsSafeDelete(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                else if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
+            }
+            catch (UnauthorizedAccessException _e)
+            {
+                _fsSafeDelete(path);
+            }
+        }
+    }
+}
--- dotnet/LearnLibs/Tests/Libgit2Test.cs
+++ dotnet/LearnLibs/Tests/Libgit2Test.cs
@@ -1,40 +1,73 @@
 ﻿using System;
 using System.IO;
 using System.Management.Automation;
-using System.Threading;
 using System.Threading.Tasks;
 using LibGit2Sharp;
 using Xunit;
+using System.Linq;
 
 namespace LearnLibs.Tests
 {
     public class Libgit2Learn
     {
-        public Libgit2Learn()
+        [Fact]
+        public async Task ShouldClone()
         {
+            if (Directory.Exists(Path.Combine(Config.RepoRoot, "clone-test")))
+            {
+                RepoUtils.FsSafeDelete(Path.Combine(Config.RepoRoot, "clone-test"));
+            }
+            using var powershell = PowerShell.Create();
+            powershell.AddScript($"cd {Config.RepoRoot}");
+            powershell.AddScript(
+                $"$env:GIT_SSH_COMMAND=\"ssh -i {Path.Combine(Config.ResourceRoot, "Ssh", "id-ed25519")}\"");
+            powershell.AddScript(
+                $@"git clone ssh://git@gitea.evernightfireworks.com:7002/lonelyhentai/probabel-test.git -b master clone-test -q");
+            powershell.AddScript("exit");
+            await powershell.InvokeAsync();
+            Assert.True(Directory.Exists(Path.Combine(Config.RepoRoot, "clone-test")));
+        }
+
+        [Fact]
+        public async Task ShouldDiff()
+        {
+            await ShouldClone();
+            using var repo = new Repository(Path.Combine(Config.RepoRoot,"clone-test"));
+            var currentCommit = repo.Lookup<Commit>("b11f6bc38ff60b10d61ee9d99d8c0b6959fdcb96");
+            var lastCommit = repo.Lookup<Commit>("77e8c01d821d626ddc09c913bbc28de068c31ffd");
+            var differences = repo.Diff.Compare<Patch>(currentCommit.Tree, lastCommit.Tree);
+            Assert.True(differences.FirstOrDefault(e => e.Path.Contains("i18n-src.js"))!=null);
         }
 
         [Fact]
         public void ShouldInit()
         {
             var repoPath = Path.Combine(Config.RepoRoot, "init-test");
-            if(Directory.Exists(repoPath)) Directory.Delete(repoPath,recursive:true);
+            if (Directory.Exists(repoPath)) RepoUtils.FsSafeDelete(repoPath);
             Repository.Init(repoPath, false);
             Assert.True(Directory.Exists(Path.Combine(repoPath, ".git")));
         }
 
         [Fact]
-        public async Task ShouldClone()
+        public async Task ShouldCommit()
         {
-            using var powershell = PowerShell.Create();
-            powershell.AddScript($"cd {Config.RepoRoot}");
-            powershell.AddScript(
-                $"$env:GIT_SSH_COMMAND=\"ssh -i {Path.Combine(Config.ResourceRoot, "Ssh", "id-ed25519")}\"");
-            powershell.AddScript(
-                $@"git clone git@github.com:lonelyhentai/enfw-plugins.git -b master clone-test -q");
-            powershell.AddScript("exit");
-            await powershell.InvokeAsync();
-            Assert.True(Directory.Exists(Path.Combine(Config.RepoRoot, "clone-test")));
+            await ShouldClone();
+            ShouldInit();
+            using var sourceRepo = new Repository(Path.Combine(Config.RepoRoot, "clone-test"));
+            using var targetRepo = new Repository(Path.Combine(Config.RepoRoot, "init-test"));
+            var lastCommit = sourceRepo.Lookup<Commit>("77e8c01d821d626ddc09c913bbc28de068c31ffd");
+            sourceRepo.Reset(ResetMode.Hard, lastCommit);
+            RepoUtils.FsSafeDelete(Path.Combine(Config.RepoRoot, "init-test", "zh-cn"));
+            RepoUtils.FsSafeCopy(Path.Combine("mix_i18n", "i18n-src.js"), Path.Combine(Config.RepoRoot, "clone-test"),
+                Path.Combine(Config.RepoRoot, "init-test","zh-cn"));
+            RepoUtils.FsSafeCopy(Path.Combine("pure_i18n", "i18n-zh-cn.json"), Path.Combine(Config.RepoRoot, "clone-test"),
+                Path.Combine(Config.RepoRoot, "init-test","zh-cn"));
+            Commands.Stage(targetRepo, "*");
+            var author = new LibGit2Sharp.Signature("lonelyhentai", "master@evernightfireworks.com", DateTime.Now);
+            var committer = author;
+            var commit = targetRepo.Commit("Here's a commit i made!", author, committer);
+            var currentCommit = sourceRepo.Lookup<Commit>("b11f6bc38ff60b10d61ee9d99d8c0b6959fdcb96");
+            sourceRepo.Reset(ResetMode.Hard, currentCommit);
         }
     }
 }
