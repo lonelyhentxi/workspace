6bcea60a440987917cc5541560bf90fa8cec4562
lonelyhentai <master@evernightfireworks.com>
2020-07-09 22:11:25 +0800

    feat: start learn recycler view

---
+++ jvm/FirstCodeAndroid/RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerview.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.collections.ArrayList
+
+class MainActivity : AppCompatActivity() {
+
+    private val fruitNames = listOf(
+        "Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape", "Pineapple",
+        "Strawberry", "Cherry", "Mango", "Apple", "Banana", "Orange", "Watermelon",
+        "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango"
+    )
+    private var fruitList = ArrayList<Fruit>()
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initFruits()
+        // use linearLayoutManager when actually use constraintLayout as well
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        val adapter = FruitAdapter(fruitList)
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun initFruits() {
+        fruitNames.forEach {
+            fruitList.add(
+                Fruit(
+                    it,
+                    resources.getIdentifier("${it.toLowerCase(Locale.ENGLISH)}_pic", "drawable", packageName)
+                )
+            )
+        }
+    }
+}
